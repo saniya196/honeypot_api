9e618c50fdb3d32f86d052041745956bf1f263c8
Saniya <saniyaasreen196@gmail.com>
2026-02-03 21:45:50 +0530

    honeypot api

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Header, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+API_KEY = "mysecretkey123"
+
+class Message(BaseModel):
+    session_id: str
+    message: str
+
+@app.post("/honeypot")
+def honeypot(data: Message, x_api_key: str = Header(None)):
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API key")
+
+    return {
+        "scam_detected": False,
+        "status": "ok",
+        "reply": "Hello, can you explain more?",
+        "session_id": data.session_id
+    }
